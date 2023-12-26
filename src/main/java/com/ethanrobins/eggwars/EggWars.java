66a1a3ee7a995e816c0d1500c67f355bcef7fcc1
admin <ethan@shawnrobins.com>
2023-12-25 23:29:49 -0800

    Initial commit

---
+++ src/main/java/com/ethanrobins/eggwars/EggWars.java
@@ -0,0 +1,17 @@
+package com.ethanrobins.eggwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EggWars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
